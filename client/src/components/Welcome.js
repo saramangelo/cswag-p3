622c1ef42b9b42d84bd669005ca5bdaa27ff7cf4
saramangelo <saramangelo@gmail.com>
2023-02-10 08:57:13 -0800

    got modal functioning

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -2,10 +2,12 @@ import Card from "react-bootstrap/Card";
 import TicketModal from "./TicketModal";
 
 function Welcome() {
-  <>
+  return (
+    <>
     <Card body>Welcome to your Dashboard!</Card>
-    <TicketModal />
-  </>;
+    <TicketModal/>
+    </>
+  )
 }
 
 export default Welcome;
