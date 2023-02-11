71d791a0a94605ebdf3e8df9188bb28e3a6e12e6
William Voigt <wvoigt@Williams-Air.lan1>
2023-02-11 09:44:58 -0800

    added route for notfound

--- client/src/App.js
+++ client/src/App.js
@@ -5,29 +5,29 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 // import "./index.css";
-
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
   console.log(authLink);
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   console.log(token);
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -41,16 +41,17 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-    <Router>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
     </ApolloProvider>
   );
 }
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,15 +2,10 @@ import React from "react";
 import LoginCard from "../components/LoginCard";
 import Container from "react-bootstrap/Container";
 // import background from "../images/BackgroundLog.jpg";
-import background from '../images/bg4.jpg'
-
 
 const Login = () => {
   return (
-    <div
-      className=""
-      style={styles.image}
-    >
+    <div>
       <Container className="display-flex">
         <LoginCard />
       </Container>
