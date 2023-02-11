a0077eeeab49c490bb4e05b9bda944b13c2b3966
saramangelo <saramangelo@gmail.com>
2023-02-11 00:03:48 -0800

    updated login page font and bg

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -2,7 +2,7 @@ import React from "react";
 import LoginCard from "../components/LoginCard";
 import Container from "react-bootstrap/Container";
 // import background from "../images/BackgroundLog.jpg";
-import background from '../images/bg3.jpg'
+import background from '../images/bg4.jpg'
 
 const styles = {
     image: {
