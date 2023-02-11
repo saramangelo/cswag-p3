c680a75dfd212d38d31194e096ceeb950cb683e6
saramangelo <saramangelo@gmail.com>
2023-02-11 00:23:56 -0800

    updated styling

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -3,20 +3,20 @@ import TicketModal from "./TicketModal";
 
 const styles = {
   header: {
-  fontFamily: 'Rubik Mono One, sans-serif'
-
-}
-}
+    fontFamily: "Rubik Mono One, sans-serif",
+    fontSize: "40px"
+  },
+};
 
 function Welcome() {
   return (
     <>
-    <Card body >
-      <header style={styles.header} >Welcome to your Dashboard!</header>
+      <Card body>
+        <header style={styles.header}>Welcome to your Dashboard!</header>
       </Card>
-    <TicketModal/>
+      <TicketModal />
     </>
-  )
+  );
 }
 
 export default Welcome;
