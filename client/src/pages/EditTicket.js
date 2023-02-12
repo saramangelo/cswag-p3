698fb259ccf9d1c6ad316c73f3bbeeed00a1b339
William Voigt <wvoigt@Williams-Air.lan1>
2023-02-12 15:24:39 -0800

    merging

---
+++ client/src/pages/EditTicket.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const EditTicket = () => {
+  return (
+    <div>
+      <Container className="display-flex"></Container>
+    </div>
+  );
+};
+
+export default EditTicket;
