ce7dc49e8b31ec84ca1a45988f0bbf1da9b0824f
William Voigt <wvoigt@Williams-MacBook-Air.local>
2023-02-14 14:07:31 -0800

    page protection added to dashboard and view ticket

--- client/src/pages/ViewTicket.js
+++ client/src/pages/ViewTicket.js
@@ -5,8 +5,12 @@ import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { QUERY_SINGLE_TICKET } from "../utils/queries";
 import Spinner from "../components/Spinner";
+import ProtectPage from "../components/ProtectPage";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import AuthService from "../utils/auth";
+
+const auth = AuthService;
 
 const ViewTicket = () => {
   const { ticketId } = useParams();
@@ -36,43 +40,56 @@ const ViewTicket = () => {
   }).format(ticket.updatedAt);
   return (
     <>
-      <Navbar />
-      <Card className="text-center">
-        <Card.Header>Ticket Details</Card.Header>
-        <Card.Body>
-          {loading ? (
-            <Spinner />
-          ) : (
-            <>
-              <ListGroup>
-                <ListGroup.Item>Title: {ticket.ticketTitle}</ListGroup.Item>
-                <ListGroup.Item>
-                  Description: {ticket.ticketDescription}
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  Submitter: {ticket.ticketAuthor}
-                </ListGroup.Item>
-                <ListGroup.Item>Status: {ticket.ticketStatus}</ListGroup.Item>
-                <ListGroup.Item>
-                  Priority: {ticket.ticketPriority}
-                </ListGroup.Item>
-                <ListGroup.Item>Created at: {createdAt}</ListGroup.Item>
-                <ListGroup.Item>Updated at: {updatedAt}</ListGroup.Item>
-              </ListGroup>
-            </>
-          )}
-        </Card.Body>
-        <Card.Footer className="text-muted"></Card.Footer>
-      </Card>
-      <Card.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Add a comment</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-            <Button variant="outline-dark">Submit</Button>
-          </Form.Group>
-        </Form>
-      </Card.Body>
+      {auth.loggedIn() ? (
+        <>
+          <Navbar />
+          <Card className="text-center">
+            <Card.Header>Ticket Details</Card.Header>
+            <Card.Body>
+              {loading ? (
+                <Spinner />
+              ) : (
+                <>
+                  <ListGroup>
+                    <ListGroup.Item>Title: {ticket.ticketTitle}</ListGroup.Item>
+                    <ListGroup.Item>
+                      Description: {ticket.ticketDescription}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      Submitter: {ticket.ticketAuthor}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      Status: {ticket.ticketStatus}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      Priority: {ticket.ticketPriority}
+                    </ListGroup.Item>
+                    <ListGroup.Item>Created at: {createdAt}</ListGroup.Item>
+                    <ListGroup.Item>Updated at: {updatedAt}</ListGroup.Item>
+                  </ListGroup>
+                </>
+              )}
+            </Card.Body>
+            <Card.Footer className="text-muted"></Card.Footer>
+          </Card>
+          <Card.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Add a comment</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+                <Button variant="outline-dark">Submit</Button>
+              </Form.Group>
+            </Form>
+          </Card.Body>
+        </>
+      ) : (
+        <div>
+          <ProtectPage />
+        </div>
+      )}
     </>
   );
 };
