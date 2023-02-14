2b98325ea9de2f8361e69c963a28d2a5681d6263
saramangelo <saramangelo@gmail.com>
2023-02-14 14:07:07 -0800

    updated add comment ui

--- client/src/pages/ViewTicket.js
+++ client/src/pages/ViewTicket.js
@@ -7,6 +7,8 @@ import { QUERY_SINGLE_TICKET } from "../utils/queries";
 import Spinner from "../components/Spinner";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Collapse from 'react-bootstrap/Collapse';
+import { useState } from 'react';
 
 const ViewTicket = () => {
   const { ticketId } = useParams();
@@ -34,6 +36,10 @@ const ViewTicket = () => {
     minute: "2-digit",
     second: "2-digit",
   }).format(ticket.updatedAt);
+
+  // state variable for comment button collapse
+  const [open, setOpen] = useState(false);
+  
   return (
     <>
       <Navbar />
@@ -64,15 +70,29 @@ const ViewTicket = () => {
         </Card.Body>
         <Card.Footer className="text-muted"></Card.Footer>
       </Card>
-      <Card.Body>
+      <Button
+      variant="outline-dark" 
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+      >
+        Add a comment
+      </Button>
+      <div style={{ minHeight: '150px' }}>
+        <Collapse in={open} dimension="width">
+          <div id="example-collapse-text">
+            <Card body style={{ width: '400px' }}>
         <Form>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Add a comment</Form.Label>
+            <Form.Label></Form.Label>
             <Form.Control as="textarea" rows={3} />
             <Button variant="outline-dark">Submit</Button>
           </Form.Group>
         </Form>
-      </Card.Body>
+      </Card>
+          </div>
+        </Collapse>
+      </div>
     </>
   );
 };
