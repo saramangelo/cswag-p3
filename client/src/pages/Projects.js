eaf99a32ba80a8348f1b6bec5c3be9b2d86ba08b
saramangelo <saramangelo@gmail.com>
2023-02-14 22:57:29 -0800

    created project table and projects page

---
+++ client/src/pages/Projects.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import DashboardTable from "../components/DashboardTable";
+import { useQuery } from "@apollo/client";
+import { QUERY_PROJECTS } from "../utils/queries";
+import TicketModal from "../components/TicketModal";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Spinner from "../components/Spinner";
+import ProtectPage from "../components/ProtectPage";
+import Sidebar from "../components/Sidebar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AuthService from "../utils/auth";
+
+const auth = AuthService;
+
+const styles = {
+  header: {
+    fontFamily: "Rubik Mono One, sans-serif",
+    fontSize: "30px"
+  },
+  button: {
+    fontFamily: "Rubik Mono One, sans-serif",
+  },
+};
+
+function Projects({handleShow, handleClose, show}) {
+  const { loading, data } = useQuery(QUERY_PROJECTS, {
+    onCompleted: () => {
+      setProjectData(data.projects);
+    },
+  });
+
+  const [projectData, setProjectData] = useState([]);
+
+
+  // TicketAuthor from getProfile (current user)
+  const currentUser = auth.getProfile().data;
+  console.log(currentUser);
+
+  return (
+      <>
+      {auth.loggedIn() ? (
+        <Container fluid className="body-container">
+          <Sidebar handleShow={handleShow}/>
+          <Row>
+            <Col xs={1} lg={2} >
+              {" "}
+            </Col>
+            <Col xs={10} lg={9}>
+              <Card body className="welcome-card">
+                  <header style={styles.header}>
+                    Welcome, {currentUser.username}!
+                  </header>
+                </Card>
+
+                <Button style={styles.button} variant="dark" onClick={handleShow}>
+                  Create a ticket
+                </Button>
+                <TicketModal projectData={projectData} setProjectData={setProjectData} currentUser={currentUser} handleClose={handleClose} show={show}/>
+
+                {loading ? (
+                  <Spinner />
+                ) : (
+                  <div>
+                    <div style={styles.header}>Current tickets</div>
+                    <DashboardTable
+                      projects={projectData}
+                      setProjectData={setProjectData}
+                    />
+                  </div>
+                )}
+            </Col>
+          </Row>
+        </Container>
+      ) : (
+        <div>
+          <ProtectPage />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Projects;
