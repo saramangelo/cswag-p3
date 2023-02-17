3220a8f9a2694ffe01462bd54580dd87fe775c80
saramangelo <saramangelo@gmail.com>
2023-02-16 18:40:03 -0800

    successfully moved spinner out of project details card on ViewProject.js

--- client/src/pages/ViewProject.js
+++ client/src/pages/ViewProject.js
@@ -12,7 +12,7 @@ import ProtectPage from "../components/ProtectPage";
 import ProjectModal from "../components/ProjectModal";
 import TicketModal from "../components/TicketModal";
 import { useState } from "react";
-import ProjectTickets from  "../components/ProjectTickets";
+import ProjectTickets from "../components/ProjectTickets";
 import AuthService from "../utils/auth";
 import AddTicketToProjectModal from "../components/AddTicketToProjectModal";
 import Button from "react-bootstrap/Button";
@@ -29,10 +29,16 @@ const styles = {
   },
 };
 
-
 const auth = AuthService;
 
-const ViewProject = ({ handleClose, handleShow, show, handleProjectShow, handleProjectClose, showProject}) => {
+const ViewProject = ({
+  handleClose,
+  handleShow,
+  show,
+  handleProjectShow,
+  handleProjectClose,
+  showProject,
+}) => {
   const { projectId } = useParams();
 
   const { loading, data } = useQuery(QUERY_SINGLE_PROJECT, {
@@ -64,24 +70,26 @@ const ViewProject = ({ handleClose, handleShow, show, handleProjectShow, handleP
   const [dashData, setDashData] = useState([]);
 
   const currentUser = auth.getProfile().data;
-  
 
   return (
     <>
       {auth.loggedIn() ? (
-        <Container fluid className="body-container">
-          <Sidebar handleShow={handleShow} handleProjectShow={handleProjectShow}/>
-          <Row>
-            <Col xs={1} lg={3}>
-              {" "}
-            </Col>
-            <Col xs={10} lg={8}>
-              <Card className="text-center detail-card">
-                <Card.Header>Project Details</Card.Header>
-                <Card.Body>
-                  {loading ? (
-                    <Spinner />
-                  ) : (
+        loading ? (
+          <Spinner />
+        ) : (
+          <Container fluid className="body-container">
+            <Sidebar
+              handleShow={handleShow}
+              handleProjectShow={handleProjectShow}
+            />
+            <Row>
+              <Col xs={1} lg={3}>
+                {" "}
+              </Col>
+              <Col xs={10} lg={8}>
+                <Card className="text-center detail-card">
+                  <Card.Header>Project Details</Card.Header>
+                  <Card.Body>
                     <>
                       <ListGroup>
                         <ListGroup.Item>
@@ -100,45 +108,46 @@ const ViewProject = ({ handleClose, handleShow, show, handleProjectShow, handleP
                         <ListGroup.Item>Updated at: {updatedAt}</ListGroup.Item>
                       </ListGroup>
                     </>
-                  )}
-                </Card.Body>
-                <Card.Footer className="text-muted"></Card.Footer>
-              </Card>
-              <Button
+                  </Card.Body>
+                  <Card.Footer className="text-muted"></Card.Footer>
+                </Card>
+                <>
+                  <Button
                     style={styles.button}
                     variant="dark"
                     onClick={handleShow}
                   >
-                    Create a Ticket for this project
+                    Create a ticket for this project
                   </Button>
-              <AddTicketToProjectModal/>
+                  <AddTicketToProjectModal />
 
-              <ProjectTickets/>
-                      {/* ICEBOX */}
-              {/* <CommentList /> */}
-
-            </Col>
-          </Row>
-        </Container>
+                  <ProjectTickets />
+                  {/* ICEBOX */}
+                  {/* <CommentList /> */}
+                </>
+              </Col>
+            </Row>
+            <TicketModal
+              dashData={dashData}
+              setDashData={setDashData}
+              currentUser={currentUser}
+              handleClose={handleClose}
+              show={show}
+            />
+            <ProjectModal
+              projectData={projectData}
+              setProjectData={setProjectData}
+              currentUser={currentUser}
+              handleProjectClose={handleProjectClose}
+              showProject={showProject}
+            />
+          </Container>
+        )
       ) : (
         <div>
           <ProtectPage />
         </div>
       )}
-      <TicketModal
-        dashData={dashData}
-        setDashData={setDashData}
-        currentUser={currentUser}
-        handleClose={handleClose}
-        show={show}
-      />
-      <ProjectModal
-        projectData={projectData}
-        setProjectData={setProjectData}
-        currentUser={currentUser}
-        handleProjectClose={handleProjectClose}
-        showProject={showProject}
-      />
     </>
   );
 };
