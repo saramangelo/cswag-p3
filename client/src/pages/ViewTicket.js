5767edcd0b929fbcc11ff0a4b6433667d8bf5295
saramangelo <saramangelo@gmail.com>
2023-02-16 19:25:06 -0800

    updated spinner

--- client/src/pages/ViewTicket.js
+++ client/src/pages/ViewTicket.js
@@ -1,57 +1,241 @@
+// import Card from "react-bootstrap/Card";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
+// import Container from "react-bootstrap/Container";
+// import Sidebar from "../components/Sidebar";
+// import { useQuery } from "@apollo/client";
+// import { useParams } from "react-router-dom";
+// import { QUERY_SINGLE_TICKET } from "../utils/queries";
+// import Spinner from "../components/Spinner";
+// import ProtectPage from "../components/ProtectPage";
+// import TicketModal from "../components/TicketModal";
+// import ProjectModal from "../components/ProjectModal";
+// import { useEffect, useState } from "react";
+
+// import AuthService from "../utils/auth";
+// import CommentList from "../components/CommentList";
+
+// const auth = AuthService;
+
+// const ViewTicket = ({
+//   show,
+//   handleShow,
+//   handleClose,
+//   showProject,
+//   handleProjectShow,
+//   handleProjectClose,
+// }) => {
+//   const [dashData, setDashData] = useState([]);
+
+//   const [projectData, setProjectData] = useState([]);
+
+//   const [commentData, setCommentData] = useState([]);
+  
+//   const { ticketId } = useParams();
+
+//   const { loading, data } = useQuery(QUERY_SINGLE_TICKET, {
+//     variables: { ticketId },
+//     onCompleted: () => {
+//       setCommentData(data.ticket.comments);
+//     },
+//   });
+
+//   const ticket = data?.ticket || [];
+
+//   // Date formatters for CreatedAt and updatedAt
+//   const ticketCreatedAt = new Intl.DateTimeFormat("en-US", {
+//     year: "numeric",
+//     month: "2-digit",
+//     day: "2-digit",
+//     hour: "2-digit",
+//     minute: "2-digit",
+//     second: "2-digit",
+//   }).format(ticket.createdAt);
+
+//   const updatedAt = new Intl.DateTimeFormat("en-US", {
+//     year: "numeric",
+//     month: "2-digit",
+//     day: "2-digit",
+//     hour: "2-digit",
+//     minute: "2-digit",
+//     second: "2-digit",
+//   }).format(ticket.updatedAt);
+
+//   // state variable for comment button collapse
+//   // const [open, setOpen] = useState(false);
+
+//   // current user data to send to Ticket Modal
+//   const currentUser = auth.getProfile().data;
+
+//   const [flairColor, setFlairColor] = useState("grey");
+
+//   let styles = {
+//     backgroundColor: flairColor,
+//   };
+
+//   useEffect(() => {
+//     if (ticket?.ticketPriority === "Low") {
+//       setFlairColor("#a5db95");
+//     } else if (ticket?.ticketPriority === "Medium") {
+//       setFlairColor("#f7f374");
+//     } else if (ticket?.ticketPriority === "High") {
+//       setFlairColor("#e88e82");
+//     } else {
+//       setFlairColor("#f288d6");
+//     }
+//   }, [ticket?.ticketPriority]);
+
+//   return (
+//     <>
+//       {auth.loggedIn() ? (
+//         loading ? (
+//           <Spinner />
+//         ) : (
+//           <Container fluid className="body-container">
+//             <Sidebar
+//               handleShow={handleShow}
+//               handleProjectShow={handleProjectShow}
+//             />
+//             <Row>
+//               <Col xs={1} lg={3}>
+//                 {" "}
+//               </Col>
+//               <Col xs={10} lg={8}>
+//                 <Card className="text-center ticket-detail-card">
+//                   <Card.Header className="ticket-detail-header">
+//                     <div className="ticket-detail-header-left">
+//                       <Card.Title className="ticket-detail-title">
+//                         {ticket.ticketTitle}
+//                       </Card.Title>
+//                       <div className="ticket-detail-type">
+//                         {ticket.ticketType}
+//                       </div>
+//                     </div>
+//                     <Card.Text className="ticket-detail-submission">
+//                       <div>Submitted by {ticket.ticketAuthor}</div>
+//                       <div>Assigned to {ticket.ticketAssignee}</div>
+//                     </Card.Text>
+//                   </Card.Header>
+//                   <Card.Body>
+//                     <Card.Text className="ticket-detail-description">
+//                       {ticket.ticketDescription}
+//                     </Card.Text>
+//                     <div className="ticket-detail-flair">
+//                       <Card.Text className="ticket-detail-status">
+//                         {ticket.ticketStatus}
+//                       </Card.Text>
+//                       <Card.Text
+//                         className="ticket-detail-priority"
+//                         style={styles}
+//                       >
+//                         {ticket.ticketPriority} Priority
+//                       </Card.Text>
+//                     </div>
+//                   </Card.Body>
+//                   <Card.Footer className="ticket-detail-footer">
+//                     {ticketCreatedAt.split(", ")[0]} [
+//                     {ticketCreatedAt.split(", ")[1].slice(0, 5)}
+//                     {ticketCreatedAt.split(", ")[1].slice(8)}]<br></br>
+//                     {updatedAt === ticketCreatedAt
+//                       ? ""
+//                       : `  (Updated ${updatedAt.split(", ")[0]} [${updatedAt
+//                           .split(", ")[1]
+//                           .slice(0, 5)}${updatedAt.split(", ")[1].slice(8)}])`}
+//                   </Card.Footer>
+//                 </Card>
+//                 <CommentList
+//                   ticketId={ticketId}
+//                   comments={commentData}
+//                   setCommentData={setCommentData}
+//                 />
+//               </Col>
+//             </Row>
+//             <TicketModal
+//               dashData={dashData}
+//               setDashData={setDashData}
+//               currentUser={currentUser}
+//               handleClose={handleClose}
+//               show={show}
+//             />
+//             <ProjectModal
+//               projectData={projectData}
+//               setProjectData={setProjectData}
+//               currentUser={currentUser}
+//               handleProjectClose={handleProjectClose}
+//               showProject={showProject}
+//             />
+//           </Container>
+//         )
+//       ) : (
+//         <div>
+//           <ProtectPage />
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default ViewTicket;
+
+
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import ListGroup from "react-bootstrap/ListGroup";
 import Sidebar from "../components/Sidebar";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
-import { QUERY_SINGLE_TICKET } from "../utils/queries";
+import { QUERY_SINGLE_PROJECT } from "../utils/queries";
 import Spinner from "../components/Spinner";
 import ProtectPage from "../components/ProtectPage";
-import TicketModal from "../components/TicketModal";
 import ProjectModal from "../components/ProjectModal";
-import { useEffect, useState } from "react";
-
+import TicketModal from "../components/TicketModal";
+import { useState } from "react";
+import ProjectTickets from "../components/ProjectTickets";
 import AuthService from "../utils/auth";
-import CommentList from "../components/CommentList";
+import AddTicketToProjectModal from "../components/AddTicketToProjectModal";
+import Button from "react-bootstrap/Button";
+// ICEBOXED COMMENT LIST
+// import CommentList from "../components/CommentList";
+
+const styles = {
+  header: {
+    fontFamily: "Rubik Mono One, sans-serif",
+    fontSize: "30px",
+  },
+  button: {
+    fontFamily: "Rubik Mono One, sans-serif",
+  },
+};
 
 const auth = AuthService;
 
-const ViewTicket = ({
-  show,
-  handleShow,
+const ViewProject = ({
   handleClose,
-  showProject,
+  handleShow,
+  show,
   handleProjectShow,
   handleProjectClose,
+  showProject,
 }) => {
-  const [dashData, setDashData] = useState([]);
+  const { projectId } = useParams();
 
-  const [projectData, setProjectData] = useState([]);
-
-  const [commentData, setCommentData] = useState([]);
-  
-  const { ticketId } = useParams();
-
-  const { loading, data } = useQuery(QUERY_SINGLE_TICKET, {
-    variables: { ticketId },
-    onCompleted: () => {
-      setCommentData(data.ticket.comments);
-    },
+  const { loading, data } = useQuery(QUERY_SINGLE_PROJECT, {
+    variables: { projectId },
   });
 
-  const ticket = data?.ticket || [];
+  const project = data?.project || [];
 
-  // Date formatters for CreatedAt and updatedAt
-  const ticketCreatedAt = new Intl.DateTimeFormat("en-US", {
+  // Date formatters for createdAt and updatedAt
+  const createdAt = new Intl.DateTimeFormat("en-US", {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
-  }).format(ticket.createdAt);
-
+  }).format(project.createdAt);
   const updatedAt = new Intl.DateTimeFormat("en-US", {
     year: "numeric",
     month: "2-digit",
@@ -59,31 +243,15 @@ const ViewTicket = ({
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
-  }).format(ticket.updatedAt);
+  }).format(project.updatedAt);
 
-  // state variable for comment button collapse
-  // const [open, setOpen] = useState(false);
+  const [projectData, setProjectData] = useState([]);
 
-  // current user data to send to Ticket Modal
+  const [dashData, setDashData] = useState([]);
+
   const currentUser = auth.getProfile().data;
 
-  const [flairColor, setFlairColor] = useState("grey");
-
-  let styles = {
-    backgroundColor: flairColor,
-  };
-
-  useEffect(() => {
-    if (ticket?.ticketPriority === "Low") {
-      setFlairColor("#a5db95");
-    } else if (ticket?.ticketPriority === "Medium") {
-      setFlairColor("#f7f374");
-    } else if (ticket?.ticketPriority === "High") {
-      setFlairColor("#e88e82");
-    } else {
-      setFlairColor("#f288d6");
-    }
-  }, [ticket?.ticketPriority]);
+  // {project.projectTitle}, {createdAt}, {updatedAt}
 
   return (
     <>
@@ -101,53 +269,39 @@ const ViewTicket = ({
                 {" "}
               </Col>
               <Col xs={10} lg={8}>
-                <Card className="text-center ticket-detail-card">
-                  <Card.Header className="ticket-detail-header">
-                    <div className="ticket-detail-header-left">
-                      <Card.Title className="ticket-detail-title">
-                        {ticket.ticketTitle}
-                      </Card.Title>
-                      <div className="ticket-detail-type">
-                        {ticket.ticketType}
-                      </div>
-                    </div>
-                    <Card.Text className="ticket-detail-submission">
-                      <div>Submitted by {ticket.ticketAuthor}</div>
-                      <div>Assigned to {ticket.ticketAssignee}</div>
-                    </Card.Text>
+                <Card className="text-center project-detail-card">
+                  <Card.Header className="project-detail-header">
+                    <Card.Title className="project-detail-title">
+                      {project.projectTitle}
+                    </Card.Title>
+                    <Card.Text>Manager: {project.projectManager}</Card.Text>
                   </Card.Header>
                   <Card.Body>
-                    <Card.Text className="ticket-detail-description">
-                      {ticket.ticketDescription}
-                    </Card.Text>
-                    <div className="ticket-detail-flair">
-                      <Card.Text className="ticket-detail-status">
-                        {ticket.ticketStatus}
+                    <>
+                      <Card.Text className="project-detail-description">
+                        {project.projectDescription}
                       </Card.Text>
-                      <Card.Text
-                        className="ticket-detail-priority"
-                        style={styles}
-                      >
-                        {ticket.ticketPriority} Priority
-                      </Card.Text>
-                    </div>
+                      <div className="project-detail-flair">
+                        <Card.Text className="project-detail-status">
+                          {project.projectStatus}
+                        </Card.Text>
+                      </div>
+                    </>
                   </Card.Body>
-                  <Card.Footer className="ticket-detail-footer">
-                    {ticketCreatedAt.split(", ")[0]} [
-                    {ticketCreatedAt.split(", ")[1].slice(0, 5)}
-                    {ticketCreatedAt.split(", ")[1].slice(8)}]<br></br>
-                    {updatedAt === ticketCreatedAt
-                      ? ""
-                      : `  (Updated ${updatedAt.split(", ")[0]} [${updatedAt
-                          .split(", ")[1]
-                          .slice(0, 5)}${updatedAt.split(", ")[1].slice(8)}])`}
-                  </Card.Footer>
+                  <Card.Footer className="text-muted"></Card.Footer>
                 </Card>
-                <CommentList
-                  ticketId={ticketId}
-                  comments={commentData}
-                  setCommentData={setCommentData}
-                />
+                <Button
+                  style={styles.button}
+                  variant="dark"
+                  onClick={handleShow}
+                >
+                  Create a Ticket for this project
+                </Button>
+                <AddTicketToProjectModal />
+
+                <ProjectTickets project={project} />
+                {/* ICEBOX */}
+                {/* <CommentList /> */}
               </Col>
             </Row>
             <TicketModal
@@ -175,4 +329,4 @@ const ViewTicket = ({
   );
 };
 
-export default ViewTicket;
+export default ViewProject;
